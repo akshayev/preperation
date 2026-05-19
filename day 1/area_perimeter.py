50fd580b99834c45a67ad29d706cab33f4399ad5
akshayev <akshayev47@gmail.com>
2026-05-20 00:15:57 +0530

    feat: add area and perimeter calculation script

---
+++ day 1/area_perimeter.py
@@ -0,0 +1,8 @@
+length = float(input("Enter the length :"))
+width = float(input("enter the width :"))
+
+area = length * width
+
+perimeter = (2 * (length + width))
+
+print(f"area is {area} and perimeter is {perimeter}")
